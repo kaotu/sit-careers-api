690fbd0164a75e9c8a39b25e550501e3b3b1a815
kaotu <kao-tu@hotmail.co.th>
2020-11-17 21:16:24 +0700

    🗃 Migration company table 🌚

---
+++ database/migrations/2020_11_17_133010_company_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CompanyTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('company', function (Blueprint $table) {
+            $table->uuid('company_id');
+            $table->string('company_name_th');
+            $table->string('company_name_en');
+            $table->string('company_type');
+            $table->string('description');
+            $table->string('about_us');
+            $table->string('logo');
+            $table->string('business_days');
+            $table->string('business_time');
+            $table->integer('tel_no');
+            $table->integer('phone_no');
+            $table->string('website');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Company');
+    }
+}
