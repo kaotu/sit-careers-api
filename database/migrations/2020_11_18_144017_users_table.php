ad562f6abbb10f24a9999600c56f93e2d6c4546e
kaotu <kao-tu@hotmail.co.th>
2020-11-18 23:20:19 +0700

    🗃 Migrate users 🌚

---
+++ database/migrations/2020_11_18_144017_users_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->uuid('user_id')->primary();
+            $table->uuid('role_id')->nullable(false);
+            $table->string('username');
+            $table->string('password');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->string('created_by');
+        });
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreign('role_id')->references('role_id')->on('roles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::disableForeignKeyConstraints();
+        Schema::dropIfExists('users');
+        Schema::enableForeignKeyConstraints();
+    }
+}
