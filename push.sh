e668a5cc787cf561c427ae9f62f83c3afc998432
kaotu <kao-tu@hotmail.co.th>
2020-12-08 22:28:36 +0700

    🎨 Update dockerfile name 🌚

--- push.sh
+++ push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo Build images..
-docker build -t 192479299400.dkr.ecr.ap-southeast-1.amazonaws.com/sit-careers-api:api-dev -f ./docker/api/Dockerfile-dev .
+docker build -t 192479299400.dkr.ecr.ap-southeast-1.amazonaws.com/sit-careers-api:api-dev -f ./docker/api/Dockerfile.dev .
 docker build -t 192479299400.dkr.ecr.ap-southeast-1.amazonaws.com/sit-careers-api:nginx-dev -f ./docker/nginx/Dockerfile .
 
 echo Push images..
