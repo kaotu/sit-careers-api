f45fe8c579885f9c58a7ef740fa423f8763ddeef
kaotu <kao-tu@hotmail.co.th>
2021-01-14 15:56:35 +0700

    ✅  Add create announcement success 🌚

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,15 +7,19 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 use App\Models\Company;
+use App\Models\JobPosition;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication, DatabaseTransactions;
 
     protected $faker;
+    protected $fakerJobPosition;
+
 
     public function setUp() :void {
         parent::setUp();
         $this->faker = factory(Company::class)->create();
+        $this->fakerJobPosition = factory(JobPosition::class)->create();
     }
 }
