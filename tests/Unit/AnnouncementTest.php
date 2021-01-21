6c5f89476a9c4b38c33fb059fd242a031a604e97
kaotu <kao-tu@hotmail.co.th>
2021-01-21 23:58:27 +0700

    ✅ Add fail update announcement case 🌚

--- tests/Unit/AnnouncementTest.php
+++ tests/Unit/AnnouncementTest.php
@@ -47,6 +47,7 @@ class AnnouncementTest extends TestCase
 
     public function test_post_announcement_fail_should_return_status_500()
     {
+        //Filed announcement_title missing
         $data = [
             'company_id' => $this->faker->company_id,
             'job_description' => 'เป็นซอฟ์ตแวร์เอน เอนแบบเอนเตอร์เทน',
@@ -95,5 +96,31 @@ class AnnouncementTest extends TestCase
 
         $this->assertEquals($announcement_arr['announcement_id'], $response_arr['announcement_id']);
         $this->assertEquals($announcement_arr['job_description'], $response_arr['job_description']);
+    }
+
+    public function test_update_announcement_fail_should_return_status_500()
+    {
+        $jobType = factory(JobType::class)->create([
+            "announcement_id" => $this->fakerAnnouncement->announcement_id,
+            "job_id" => Uuid::uuid()
+        ]);
+
+        //Filed announcement_id (pk) missing
+        $data = [
+            'company_id' => $this->faker->company_id,
+            'announcement_title' => 'รับสมัครงานตำแหน่ง UX/UI',
+            'job_description' => 'ต้องการ UX/UI',
+            'job_position_id' => $this->fakerJobPosition->job_position_id,
+            'property' => 'ขยันเป็นพอ',
+            'picture' => 'path/picture',
+            'start_date' => '2021-01-10 13:00:00',
+            'end_date' => '2021-03-31 17:00:00',
+            'welfare' => 'เงินดี ไม่ต้องแย่งลงทะเบียน',
+            'status' => 'เปิดรับสมัคร',
+            'job_type' => 'WiL'
+        ];
+
+        $response = $this->putJson('api/academic-industry', $data);
+        $response->assertStatus(500);
     }
 }
