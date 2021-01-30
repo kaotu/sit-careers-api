f1460c79677fbbd452c3fe8e93507abb7db1d63d
mild-tn <tassaneewan.mild@mail.kmutt.ac.th>
2021-01-31 01:13:24 +0700

    :wrench:  Fix comand php artisan migrate:fresh --seed 🌻

--- database/seeds/JobPositionsSeeder.php
+++ database/seeds/JobPositionsSeeder.php
@@ -1,8 +1,9 @@
 <?php
 
-use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 class JobPositionsSeeder extends Seeder
 {
