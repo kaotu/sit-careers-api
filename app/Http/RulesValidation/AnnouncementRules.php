4062fe076b050e582d674680e86eae97880a6b9e
kaotu <kao-tu@hotmail.co.th>
2021-02-02 00:43:15 +0700

    Update validate data 🌚

--- app/Http/RulesValidation/AnnouncementRules.php
+++ app/Http/RulesValidation/AnnouncementRules.php
@@ -17,10 +17,18 @@ trait AnnouncementRules
         'welfare' => 'required|string',
         'status' => 'required|string',
         'job_type' => 'required|string',
+        'address_one' => 'required|string',
+        'address_two' => 'nullable|string',
+        'lane' => 'nullable|string',
+        'road' => 'nullable|string',
+        'sub_district' => 'required|string',
+        'district' => 'required|string',
+        'province' => 'required|string',
+        'postal_code' => 'required|string',
         'start_business_day' => 'required|string|max:20',
         'end_business_day' => 'required|string|max:20',
         'start_business_time' => 'required|string|max:6',
-        'end_business_time' => 'required|string|max:6',
+        'end_business_time' => 'required|string|max:6'
     ];
 
     private $rulesGetAnnouncementById = [
@@ -28,6 +36,28 @@ trait AnnouncementRules
     ];
 
     private $rulesUpdateAnnouncementById = [
-        'announcement_id' => 'required|string'
+        'announcement_id' => 'required|string',
+        'announcement_title' => 'required|string',
+        'job_description' => 'required|string',
+        'property' => 'required|string',
+        'picture' => 'nullable|mimes:jpeg,jpg,png,gif|max:50000',
+        'start_date' => 'required|string',
+        'end_date' => 'required|string',
+        'salary' => 'required|string',
+        'welfare' => 'required|string',
+        'status' => 'required|string',
+        'job_type' => 'required|string',
+        'address_one' => 'required|string',
+        'address_two' => 'nullable|string',
+        'lane' => 'nullable|string',
+        'road' => 'nullable|string',
+        'sub_district' => 'required|string',
+        'district' => 'required|string',
+        'province' => 'required|string',
+        'postal_code' => 'required|string',
+        'start_business_day' => 'required|string|max:20',
+        'end_business_day' => 'required|string|max:20',
+        'start_business_time' => 'required|string|max:6',
+        'end_business_time' => 'required|string|max:6'
     ];
 }
