43dc80839d8f1ac17ffc82e3d876b42656201536
kaotu <kao-tu@hotmail.co.th>
2021-02-11 22:43:31 +0700

    🗃 Update relataion of user and history 🌚

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,11 +19,16 @@ class User extends Model
     protected $keyType = 'uuid';
 
     const CREATED_AT = 'created_at';
-    const UPDATED_AT = 'updated_at'; 
+    const UPDATED_AT = 'updated_at';
     const DELETED_AT = 'deleted_at';
 
     public function role()
     {
         return $this->hasOne('App\Models\Role', 'role_id');
+    }
+
+    public function history()
+    {
+        return $this->belongsTo('App\Models\History', 'history_id');
     }
 }
