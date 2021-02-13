1e4682945da1f82385cacb5f7d8627e4c43583b9
kaotu <kao-tu@hotmail.co.th>
2021-02-13 21:35:02 +0700

    🗃 Add banner table 🌚

---
+++ database/migrations/2021_02_13_141829_banners_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class BannersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('banners', function (Blueprint $table) {
+            $table->bigInteger('banner_id')->primary();
+            $table->string('path_image')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('banners');
+        Schema::table('banners', function (Blueprint $table){
+            $table->dropSoftDeletes();
+        });
+    }
+}
