85a1561a070b9090925bea7aed81fc39e137e76f
kaotu <kao-tu@hotmail.co.th>
2021-02-14 13:31:05 +0700

    ♿️ Set new bannerName 🌚

--- app/Http/Controllers/BannerController.php
+++ app/Http/Controllers/BannerController.php
@@ -2,15 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use Validator;
 use Storage;
 
 use Illuminate\Http\Request;
 
 use App\Http\Controllers\Controller as Controller;
+use App\Http\RulesValidation\BannerRules;
 use App\Repositories\BannerRepositoryInterface;
+use Throwable;
 
 class BannerController extends Controller
 {
+    use BannerRules;
     private $banner;
 
     public function __construct(BannerRepositoryInterface $banner_repo)
@@ -35,14 +39,22 @@ class BannerController extends Controller
     public function create(Request $request)
     {
         $data = request()->all();
-        $s3 = Storage::disk('s3');
-        $bannerName = time(). '.' . $data['file_banner']->getClientOriginalName();
-        $file = $request->file('file_banner');
-        if (!is_null($file)) {
-            $uploaded = $s3->put('/banner/'.$bannerName, file_get_contents($file), 'public');
-            $data['path_image'] = $bannerName;
+        $validated = Validator::make($data, $this->ruleCreationBanner);
+        if ($validated->fails()) {
+            return response()->json($validated->messages(), 400);
         }
-        $banner = $this->banner->createBanner($data);
+        try {
+            $s3 = Storage::disk('s3');
+            $bannerName = str_replace(' ', '-', time().'_'.rand(10000,99999));
+            $file = $request->file('file_banner');
+            if (!is_null($file)) {
+                $uploaded = $s3->put('/banner/'.$bannerName, file_get_contents($file), 'public');
+                $data['path_image'] = $bannerName;
+            }
+            $banner = $this->banner->createBanner($data);
+        } catch (Throwable $e) {
+            return "Something Wrong: ".$e;
+        }
         return response()->json($banner, 200);
     }
 
