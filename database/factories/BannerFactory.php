c6d287aaeb593cdd206f4b0f39b06e98cd51d613
kaotu <kao-tu@hotmail.co.th>
2021-02-14 13:32:02 +0700

    Add banner factory 🌚

---
+++ database/factories/BannerFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Banner;
+use Faker\Generator as Faker;
+
+$factory->define(Banner::class, function (Faker $faker) {
+    return [
+        'banner_id' => rand(),
+        'path_image' => '-'
+    ];
+});
